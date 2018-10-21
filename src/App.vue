069e28f069cbbe11aac69e52bf78c1f2f70d5716
AlexeyTopchiev <wearelols001@gmail.com>
2018-10-22 01:08:27 +0300

    Passing values and events from the parent component to the child

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,36 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <h1>Parent: {{ pizzaName }}</h1>
+    <Pizza  :pizzaName="pizzaName"
+            :pizzaPrice="pizzaPrice"
+            @changedName="pizzaName = $event"
+            :changeFunc="changeNameToPizza"
+          />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Pizza from './components/Pizza.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    Pizza
+  },
+  data() {
+    return {
+      pizzaName: 'Дон Бекон',
+      pizzaPrice: 300
+    }
+  },
+  methods: {
+    changeNameToPizza() {
+      this.pizzaName = 'ChickenBarbecu'
+    }
   }
 }
 </script>
@@ -21,7 +40,7 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
   margin-top: 60px;
 }
---
+++ src/components/Pizza.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <h1>Pizza Name: {{ pizzaName }} / {{ reverseName }}</h1>
+        <h2>Price: {{ pizzaPrice }} </h2>
+        <button @click="changeName">Change Name</button>
+        <button @click="changeFunc()">Change Name from Parent</button>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'pizza',
+    props: ['pizzaName', 'pizzaPrice', 'changeFunc'],
+    // props: {
+    //     pizzaName: String,
+    //     pizzaPrice: Number
+    // },
+    // props: {
+    //     pizzaName: {
+    //         type: String,
+    //         required: true,
+    //         default: "Default Pizza Name"
+    //     },
+    //     pizzaPrice: {
+    //         type: Number
+    //     }
+    // },
+    methods: {
+        changeName () {
+            this.pizzaName = 'Маргарита'
+            this.$emit('changedName', this.pizzaName)
+        }
+    },
+    computed: {
+        reverseName () {
+            return this.pizzaName.split('').reverse().join('')
+        }
+    }
+}
+</script>
+
